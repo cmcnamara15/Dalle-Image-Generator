8d8dd5bf30126339a395e111269d71ed592fdf9c
Chris McNamara <cmcnamara1991@icloud.com>
2023-08-09 16:40:35 -0400

    Finish post route to return image in insomnia

--- controllers/openaiControllers.js
+++ controllers/openaiControllers.js
@@ -6,9 +6,31 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const generateImage = async (req, res) => {
+    try {
+    const response = await openai.createImage({
+        prompt: 'Billy Joel on snowboard', 
+        n: 1,
+        size: '512x512'
+    }); 
+
+    const imageUrl = response.data.data[0].url
+
     res.status(200).json({
         success: true,
+        data: imageUrl
     });
+    } catch (error) {
+        if (error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data);
+        } else {
+            console.log(error.message);
+        }
+        res.status(400).json({
+            success: false,
+            error: 'image could not be generated'
+        });
+    }
 }
 
 module.exports = { generateImage };
